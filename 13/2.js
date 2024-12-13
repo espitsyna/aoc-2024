adabfe1cdd4fdb365ac34743d74a50ea6bc18cde
Ekaterina Spitsyna <ekaterina.spitsyna@webpros.com>
2024-12-13 10:45:56 +0100

    Day 13

---
+++ 13/2.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+const machines = fs.readFileSync('data.txt', 'utf8')
+    .split('\r\n\r\n')
+    .filter(Boolean)
+    .map(machine => {
+        const [buttonA, buttonB, prize] = machine.split('\r\n');
+        const [AX, AY] = buttonA.match(/[0-9]+/g);
+        const [BX, BY] = buttonB.match(/[0-9]+/g);
+        const [X, Y] = prize.match(/[0-9]+/g);
+
+        return { AX: +AX, AY: +AY, BX: +BX, BY: +BY, X: 10000000000000 + +X, Y: 10000000000000 + +Y };
+    });
+
+const tokens = machines.reduce((acc, machine) => {
+    const { AX, AY, BX, BY, X, Y } = machine;
+    const b = (AX * Y - AY * X) / (AX * BY - AY * BX);
+    if (!Number.isInteger(b)) {
+        return acc;
+    }
+    const a = (X - BX * b) / AX;
+    if (!Number.isInteger(a)) {
+        return acc;
+    }
+    return acc + a * 3 + b;
+}, 0);
+console.log(tokens);
